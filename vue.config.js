8ed235622c0344b03575f429d53695a703980733
Bush <jianghao3210@gmail.com>
2024-01-13 16:30:03 +0800

    修改vue配置文件

--- vue.config.js
+++ vue.config.js
@@ -48,6 +48,7 @@ module.exports = defineConfig({
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
+    },
+    allowedHosts: ['four2quote-frontend-2.onrender.com', 'localhost']
   }
 });
