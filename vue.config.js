e5c26edd66f4a6d6ecb7395b7fcf101fa9f816eb
Bush <36556002+BushJiang@users.noreply.github.com>
2024-01-21 00:33:32 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
 module.exports = {
   devServer: {
     allowedHosts: ['four2quote-frontend-2.onrender.com', 'localhost'],
