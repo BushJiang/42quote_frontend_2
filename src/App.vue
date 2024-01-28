98ee032a88b8384e690922778c3cdd1864e0b592
Bush <36556002+BushJiang@users.noreply.github.com>
2024-01-28 16:29:57 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,12 @@
 <template>
-  <div class="bg-white">
-    <!-- 为整个应用程序容器添加一些内边距 -->
-    <div class="container mx-auto p-4">
-      <!-- NavBar组件 -->
-      <nav-bar class="mb-8" />
-      
-      <!-- SearchBar组件，添加底部外边距 -->
 
-      <search-bar class="mb-8" @search="search" @dataReceived="handleDataReceived" @fetchError="handleFetchError"/>
-      
-      <!-- QuotesGrid组件，添加底部外边距 -->
-      <quotes-grid class="mb-16" :quotes="quotes"  />
-       <div v-if="quotes && quotes.length > 0" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <quote-card v-for="quote in quotes" :key="quote.id" :quote="quote" />
-        </div>
-     <!-- 可以在这里添加一个可选的元素，用于在 quotes 数组为空时显示 -->
-     <div v-else class="text-center">
-          <p>No quotes available.</p>
-    </div>
-      <!-- AppFooter组件 -->
-      <app-footer />
-    </div>
-  </div>
+ <router-view></router-view>
+
 </template>
 
 
 <script>
-import NavBar from './components/NavBar.vue';
-import SearchBar from './components/SearchBar.vue';
-import QuotesGrid from './components/QuotesGrid.vue';
-import AppFooter from './components/AppFooter.vue';
 import { provide, ref } from 'vue';
-
 
 export default {
 
@@ -52,10 +27,7 @@ export default {
 
   name: 'App',
   components: {
-    NavBar,
-    SearchBar,
-    AppFooter,
-    QuotesGrid
+
   },
   data() {
     return {
@@ -81,5 +53,3 @@ export default {
   }
 };
 </script>
-
-
