d0e4e521e24630b2c6502a82a92b33f86d9206a6
Bush <36556002+BushJiang@users.noreply.github.com>
2024-01-28 16:37:38 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,14 +1,18 @@
 
 
 // import NavBar from './components/NavBar.vue';
-import App from './App.vue';
-
+import HomePage from './components/HomePage.vue';
+import SearchContent from './components/SearchContent.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
  
   {
     path: '/',
-    component: App, // 你需要替换成实际的组件名称
+    component: HomePage , // 你需要替换成实际的组件名称
+  },
+  {
+    path: '/search',
+    component: SearchContent  , // 你需要替换成实际的组件名称
   },
   
 ];
