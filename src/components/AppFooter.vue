b95ac3e76f7cc705b4eea77d3e7e9fdb44f28e19
Bush <36556002+BushJiang@users.noreply.github.com>
2024-01-28 16:30:29 +0800

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,17 +1,16 @@
 <template>
   <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> 
- <footer
-  class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
-  <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
-    © 2023 Copyright:
-    <a
-      class="text-neutral-800 dark:text-neutral-400"
-      href="https://tw-elements.com/"
-      >TW elements</a
-    >
-   </div>
- </footer>
+  <footer class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left w-full h-10 flex flex-col justify-center"> <!-- 调整宽度和高度，并使用 flex 布局使文本内容垂直居中 -->
+    <div class="p-4 text-center text-neutral-700 dark:text-neutral-200 text-sm">
+      © 2024 Copyright:
+      <a class="text-neutral-800 dark:text-neutral-400" href="https://tw-elements.com/">活水工作室 · 活水金句</a>
+    </div>
+    <div class="h-8 dark:bg-neutral-700"></div>
+  </footer>
 </template>
+
+
+
 
 
 
